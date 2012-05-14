1af9f1c0b83e281a7e9331b82079f448866a7387
Cristian Goina <cgoina@5amsolutions.com>
2012-05-14 18:54:45 -0400

    implement score

--- src/com/fiveamsolutions/dojo/bowling/BowlingGame.java
+++ src/com/fiveamsolutions/dojo/bowling/BowlingGame.java
@@ -3,9 +3,14 @@ package com.fiveamsolutions.dojo.bowling;
 import java.util.Collection;
 
 public class BowlingGame {
-
+	
     public int scoreGame(Collection<Integer> rolls) {
-        
-        return -1;
+    	int score = 0;
+    	for (Integer roll : rolls) {
+    		score +=  roll;
+    	}
+        return score;
     }
+
+    
 }
