fb77c7431f936fc43420daaf523314004e27d448
Cristian Goina <cgoina@5amsolutions.com>
2012-05-14 19:02:52 -0400

    one spare

--- test/com/fiveamsolutions/dojo/bowling/BowlingGameTest.java
+++ test/com/fiveamsolutions/dojo/bowling/BowlingGameTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -18,5 +19,29 @@ public class BowlingGameTest {
         }
         int score = game.scoreGame(rolls);
         assertEquals(0, score);
+    }
+
+    @Test
+    public void allOnes() {
+    	BowlingGame game = new BowlingGame();
+        Collection<Integer> rolls = new ArrayList<Integer>();
+        for (int i = 0; i < 20; i++) {
+            rolls.add(1);
+        }
+    	int score = game.scoreGame(rolls);
+    	assertEquals(20, score);
+    }
+
+    @Test
+    public void oneSpare() {
+    	BowlingGame game = new BowlingGame();
+        List<Integer> rolls = new ArrayList<Integer>();
+    	rolls.add(4);
+    	rolls.add(6);
+    	for (int i = 2; i < 20; i++) {
+    		rolls.add(0);
+    	}
+    	int score = game.scoreGame(rolls);
+    	assertEquals(20, score);
     }
 }
